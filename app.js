1ca838489ce4edb6c6779f35169a5c22354acd0f
sandeepshenoy72 <1rn18cs093.sandeepshenoy@gmail.com>
2023-10-18 21:52:26 +0530

    Add JSX,Parcel

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-const parent = React.createElement("div",{id:"parent"},
-React.createElement("div",{id:"child"},
-[React.createElement("h1",{},"Hello from Parent"),React.createElement("h2",{},"Hello from Child")]))
+//Code for JSX
+const HeadingComponent = () => (
+    <div>
+    <h1>
+        Functional Component
+    </h1>
+    <h2>
+        Second
+    </h2>
+    </div>
 
+)
 
-const heading = React.createElement("h1",{id:"head",class:"toby"},"Hello World from React")
+const title = (
+    <h1>Inside Component</h1>
+)
+
+const TitleComponent = () => (
+    <div>
+    <h1>Main Component</h1>
+    {title}
+    <HeadingComponent/>
+    </div>
+)
+//Code for React Root
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+root.render(<TitleComponent />)
